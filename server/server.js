81a2dbc26151b1cdab1ff2d66d7bc8116bbc10e1
QuantumBit Systems <quantumbitsystems@outlook.com>
2025-08-13 14:11:40 +0300

    Add comprehensive CORS headers

--- server/server.js
+++ server/server.js
@@ -23,7 +23,9 @@ app.use(cors({
     'https://quantumbitsystems.pages.dev',
     'https://quantumbit-systems.pages.dev'
   ],
-  credentials: true 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 app.use(express.json());
 
